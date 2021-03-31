96907571ec10261d2f281752647a51bde6aedbd9
Leslie Stevens <flymaven@protonmail.com>
2021-03-31 17:50:00 -0500

    add invite_response email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,6 +28,7 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  # Don't care if the mailer can't send.
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.raise_delivery_errors = true
@@ -35,13 +36,13 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'gmail.com',
-    user_name: ENV['GMAIL_USERNAME'],
-    password: ENV['GMAIL_PASSWORD'],
-
+    user_name: ,
+    password: ,
     authentication: 'plain',
     enable_starttls_auto: true
   }
