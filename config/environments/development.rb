da6341f990e2cfaf83410b0250edc5214136dfca
Leslie Stevens <flymaven@protonmail.com>
2021-04-02 19:09:50 -0500

    remove credentials

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,8 +41,8 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'gmail.com',
-    user_name: 'flyladydev',
-    password: 'IhaveNOmemoryanymore!',
+    user_name: '',
+    password: '',
     authentication: 'plain',
     enable_starttls_auto: true
   }
