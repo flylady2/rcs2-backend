738ad6ce578af4f5e13916bd1d32237e16b30ab6
Leslie Stevens <flymaven@protonmail.com>
2021-04-06 17:38:09 -0500

    code to add variable number of choices to survey

--- app/controllers/api/v1/surveys_controller.rb
+++ app/controllers/api/v1/surveys_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SurveysController < ApplicationController
 
   def trigger
-    
+
     id = params["id"]
     @survey = Survey.find(id)
     @survey.calculate_winner
@@ -18,11 +18,24 @@ class Api::V1::SurveysController < ApplicationController
   end
 
   def create
+    #params[:choices][0]["content"] => adzuki
+    #byebug
+    choices = []
+    count = 0
+    while count < params[:choices].count
+      choices.push({content: params[:choices][count]["content"], winner: false})
+      count += 1
+    end
+    
+
+
     @survey = Survey.new(survey_params)
 
     if @survey.save
+      @choices = @survey.choices.build(choices)
 
-      @choices = @survey.choices.build([{content: params["choiceAContent"], winner: params["choiceAWinnerValue"]}, {content: params["choiceBContent"], winner: params["choiceBWinnerValue"]}, {content: params["choiceCContent"], winner: params["choiceCWinnerValue"]}, {content: params["choiceDContent"], winner: params["choiceDWinnerValue"]}])
+
+      #@choices = @survey.choices.build([{content: params[:choices][0]["content"], winner: false}, {content: params[:choices][1]["content"], winner: false}, {content: params[:choices][2]["content"], winner: false}, {content: params[:choices][3]["content"], winner: false}])
       #byebug
       @choices.each {|choice|
         choice.save}
