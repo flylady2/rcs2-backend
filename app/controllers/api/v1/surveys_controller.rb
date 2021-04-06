277c6107276873b61cfb80de66891d943c4e2e7c
Leslie Stevens <flymaven@protonmail.com>
2021-04-06 17:41:13 -0500

    clean up code

--- app/controllers/api/v1/surveys_controller.rb
+++ app/controllers/api/v1/surveys_controller.rb
@@ -18,25 +18,19 @@ class Api::V1::SurveysController < ApplicationController
   end
 
   def create
-    #params[:choices][0]["content"] => adzuki
-    #byebug
+
     choices = []
     count = 0
     while count < params[:choices].count
       choices.push({content: params[:choices][count]["content"], winner: false})
       count += 1
     end
-    
-
 
     @survey = Survey.new(survey_params)
 
     if @survey.save
       @choices = @survey.choices.build(choices)
 
-
-      #@choices = @survey.choices.build([{content: params[:choices][0]["content"], winner: false}, {content: params[:choices][1]["content"], winner: false}, {content: params[:choices][2]["content"], winner: false}, {content: params[:choices][3]["content"], winner: false}])
-      #byebug
       @choices.each {|choice|
         choice.save}
       #byebug
